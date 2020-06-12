d791a3c213c4c1f7427cc559d8e207bf45595b14
raulmanuelortiz <52513291+raulmanuelortiz@users.noreply.github.com>
2020-06-12 12:17:20 -0500

    Agregado constructor y getters/setters clase Libro

--- libro.cpp
+++ libro.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "libro.h"
+
+Libro::Libro(int _codigo, std::string _nombre, Autor _autor, std::string _fechaPublicacion, std::string _tema){
+    codigo = _codigo;
+    nombre = _nombre;
+    autor = _autor;
+    fechaPublicacion = _fechaPublicacion;
+    tema = _tema;
+}
+
+int Libro::getCodigo() const{
+    return codigo;
+}   
+
+void Libro::setCodigo(int codigo){
+    Libro::codigo = codigo;
+}   
+
+std::string Libro::getNombre() const{
+    return nombre;
+}
+
+void Libro::setNombre(std::string nombre){
+    Libro::nombre = nombre;
+}
+   
+Autor Libro::getAutor() const{
+    return autor;
+}
+    
+void Libro::setAutor(Autor autor){
+    Libro::autor = autor;
+}
+    
+std::string Libro::getFechaPublicacion() const{
+    return fechaPublicacion;
+}
+    
+void Libro::setFechaPulicacion(std::string fechaPublicacion){
+    Libro::fechaPublicacion = fechaPublicacion;
+}
+
+std::string Libro::getTema() const{
+    return tema;
+}
+    
+void Libro::setTema(std::string tema){
+    Libro::tema = tema;
+}
