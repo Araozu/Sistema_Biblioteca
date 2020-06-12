520e45df753eb88aa2dd071ba8a5fd8937ed8570
erika1616 <40575948+erika1616@users.noreply.github.com>
2020-06-12 17:46:11 -0500

    Update almacen.cpp

--- almacen.cpp
+++ almacen.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "almacen.h"
+
+Almacen::Almacen(int _cantidadLibros, int _numeroEstante, int _numeroPasillo){
+	cantidadLibros = _cantidadLibros;
+	numeroEstante = _numeroEstante;
+	numeroPasillo = _numeroPasillo;
+	
+}
+int Almacen::getCantidadLibros()const{
+return cantidadLibros;
+
+}
+void Almacen :: setCantidadLibros(int cantidadLibros){
+	Almacen ::cantidadLibros=cantidadLibros;
+}
+
+int Almacen :: getNumeroEstante()const{
+return numeroEstante;
+}
+void Almacen::setNumeroEstante(int numeroEstante){
+	Almacen::numeroEstante = numeroEstante;
+}
+int Almacen::getNumeroPasillo()const{
+return numeroPasillo;
+}
+void Almacen::setNumeroPasiillo(int numeroPasillo){
+	Almacen::numeroPasillo = numeroPasillo;
+}
