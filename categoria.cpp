c9e5d596dad8dfdfdef2e902996dd8559e32ff03
Rodrigo Sebastian <64474570+RodrigoSHM1999@users.noreply.github.com>
2020-06-18 22:49:11 -0500

    Add files via upload

---
+++ categoria.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Dell on 18/06/2020.
+//
+
+#include "categoria.h"
+Categoria::Categoria(std::string _nombre,int _id){
+    nombre=_nombre;
+    id=_id;
+}
+std::string Categoria::getNombre() const{
+    return nombre;
+}
+
+void Categoria::setNombre(std::string nombre){
+    Categoria::nombre = nombre;
+}
+int Categoria::getId() const{
+    return id;
+}
