2337e4d6a625a13d259964af3007702ce390d266
Araozu <tbyuyfcjjll@gmail.com>
2020-06-22 22:06:51 -0500

    Agregada funcion para separar strings

---
+++ Utils/utils.h
@@ -0,0 +1,12 @@
+//
+// Created by Fernando on 22/06/2020.
+//
+
+#ifndef SISTEMABIBLIOTECA_UTILS_H
+#define SISTEMABIBLIOTECA_UTILS_H
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> split(std::string strToSplit, char delimeter);
+
+#endif //SISTEMABIBLIOTECA_UTILS_H
