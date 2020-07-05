87a6350984bc11f83f7afc8efd39154ab8b91ba8
raulmanuelortiz <52513291+raulmanuelortiz@users.noreply.github.com>
2020-07-04 20:22:38 -0500

    Update sistema.cpp

--- sistema.cpp
+++ sistema.cpp
@@ -7,3 +7,22 @@
 Sistema::Sistema() {
     // prestamos = Sistema::cargarPrestamos();
 }
+
+static std::vector<Libro> Sistema::cargarLibros(){
+    ifstream lectura("/data/libros.csv");
+    std::vector<Libro> libros;
+    std::string line;
+    std::getline(lectura,line);
+    std::stringstream          lineStream(line);
+    std::string cell;
+    while(std::getline(lineStream,cell, ','))
+    {
+        libros.push_back(cell);
+    }
+    if (!lineStream && cell.empty())
+    {
+        libros.push_back("");
+    }
+    return libros;
+}
+
